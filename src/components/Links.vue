e9d510974fbfcee826b19a4d203427f223474744
Amaze <38401250+ylamaze@users.noreply.github.com>
2024-08-21 22:18:56 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Heartbeat, Robot, BattleNet, Cat, Cloud} from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Heartbeat, Robot, BattleNet, Cat} from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper";
@@ -74,7 +74,6 @@ const siteIcon = {
   Robot,
   BattleNet,
   Cat,
-  Cloud,
 };
 
 // 链接跳转
